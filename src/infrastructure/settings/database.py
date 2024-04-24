f103f3547ae80e515b76edf76d2f9cbbc1aafd74
aliseylaneh <aliseylaneh@gmail.com>
2024-04-24 20:20:11 +0330

    changed settings for database

--- src/infrastructure/settings/database.py
+++ src/infrastructure/settings/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-from src.infrastructure.settings.settings import SQLALCHEMY_DATABASE_URL
+from .settings import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- src/infrastructure/settings/settings.py
+++ src/infrastructure/settings/settings.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 ENVIRONMENT = os.getenv('environment')
-
+SQLALCHEMY_DATABASE_URL = "postgresql://novapardaz:novapardaz@1234@localhost:5432/store"
 match ENVIRONMENT:
     case 'production':
         SQLALCHEMY_DATABASE_URL = 'postgresql://novapardaz:novapardaz@1234@store-db.novapardaz.svc:5432/store'
