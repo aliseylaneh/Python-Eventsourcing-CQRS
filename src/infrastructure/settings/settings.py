4e6dac90104d74ebc63b12848d18ea5f0a2a3e84
aliseylaneh <aliseylaneh@gmail.com>
2024-04-26 01:12:01 +0330

    chagned thing about env

--- src/infrastructure/settings/settings.py
+++ src/infrastructure/settings/settings.py
@@ -4,9 +4,4 @@ import os
 
 load_dotenv()
 ENVIRONMENT = os.getenv('environment')
-SQLALCHEMY_DATABASE_URL = "postgresql://novapardaz:novapardaz@1234@localhost:5432/store"
-match ENVIRONMENT:
-    case 'production':
-        SQLALCHEMY_DATABASE_URL = 'postgresql://novapardaz:novapardaz@1234@store-db.novapardaz.svc:5432/store'
-    case 'local':
-        SQLALCHEMY_DATABASE_URL = 'postgresql://novapardaz:novapardaz@1234@localhost:5432/store'
+SQLALCHEMY_DATABASE_URL = 'postgresql://app_store:1234@localhost:5432/store'
