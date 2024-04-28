e9143e95363f5c9f436a05633cf321d2f7a9de5b
aliseylaneh <aliseylaneh@gmail.com>
2024-04-28 22:08:58 +0330

    store respository module name changed

--- src/infrastructure/repositories/store.py
+++ src/infrastructure/repositories/store.py
@@ -1,13 +1,14 @@
-import uuid
-from typing import Any
-from ..settings.database import get_db_session
-from ..models.store import Store
 from presentation.schema import CreateStore, UpdateStore
+from sqlalchemy import BinaryExpression, select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from ..models.store import Store
+from ..settings.database import get_db_session
+
 
 class StoreCommandRepository:
     def __init__(self):
-        self.db_session:AsyncSession = get_db_session()
+        self.db_session: AsyncSession = get_db_session()
 
     async def create(self, store: CreateStore):
         new_store = Store(name=store.name, address=store.address)
@@ -23,13 +24,16 @@ class StoreCommandRepository:
 
 class StoreQueryRepository:
     def __init__(self):
-        self.db_session:AsyncSession = get_db_session()
+        self.db_session: AsyncSession = get_db_session()
 
     async def find_by_id(self, pk: int) -> Store:
-        return await self.db.query(Store).filter(Store.id == pk).first()
+        return await self.db_session.query(Store).filter(Store.id == pk).first()
 
     async def all(self, skip: int = 0, limit: int = 20):
-        return await self.db.query(Store).offset(skip).limit(limit).all()
+        return await self.db_session.query(Store).offset(skip).limit(limit).all()
 
-    async def filter_by_fields(self, data: dict[str, Any]):
-        pass
+    async def filter_by_fields(self, *expressions: BinaryExpression):
+        query = select(Store)
+        if expressions:
+            query = query.where(*expressions)
+        return list(await self.db_session.scalars(query))
