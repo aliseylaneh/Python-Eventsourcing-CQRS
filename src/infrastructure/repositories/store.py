7fe3ec36b7552a65a663d9ccf7c135648b0f85a8
aliseylaneh <aliseylaneh@gmail.com>
2024-04-28 18:39:49 +0330

    change store info repository name into store

--- src/infrastructure/repositories/store.py
+++ src/infrastructure/repositories/store.py
@@ -2,12 +2,14 @@ import uuid
 from typing import Any
 from ..settings.database import get_db_session
 from ..models.store import Store
-from presentation.schema import CreateStore, UpdateStore
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from ...presentation.schema import CreateStore, UpdateStore
+
 
 class StoreCommandRepository:
     def __init__(self):
-        self.db_session:AsyncSession = get_db_session()
+        self.db_session: AsyncSession = get_db_session()
 
     async def create(self, store: CreateStore):
         new_store = Store(name=store.name, address=store.address)
@@ -23,13 +25,13 @@ class StoreCommandRepository:
 
 class StoreQueryRepository:
     def __init__(self):
-        self.db_session:AsyncSession = get_db_session()
+        self.db_session: AsyncSession = get_db_session()
 
     async def find_by_id(self, pk: int) -> Store:
-        return await self.db.query(Store).filter(Store.id == pk).first()
+        return await self.db_session.query(Store).filter(Store.id == pk).first()
 
     async def all(self, skip: int = 0, limit: int = 20):
-        return await self.db.query(Store).offset(skip).limit(limit).all()
+        return await self.db_session.query(Store).offset(skip).limit(limit).all()
 
     async def filter_by_fields(self, data: dict[str, Any]):
         pass
--- src/services/store.py
+++ src/services/store.py
@@ -1,6 +1,6 @@
 import uuid
 
-from infrastructure.repositories.store_info import StoreQueryRepository
+from src.infrastructure.repositories.store import StoreQueryRepository
 
 
 class BaseStoreQuery:
