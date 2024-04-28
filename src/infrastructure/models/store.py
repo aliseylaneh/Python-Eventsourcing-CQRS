7437920e541c91b7688cc17b15b5f33189a8a7f7
aliseylaneh <aliseylaneh@gmail.com>
2024-04-28 23:54:30 +0330

    added autoincrement into store id table

--- src/infrastructure/models/store.py
+++ src/infrastructure/models/store.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class Store(Base):
     __tablename__ = "stores"
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(50), nullable=False, unique=True)
     address = Column(String(255), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
