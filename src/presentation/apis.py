c34e125fadbff4e4b14f577ca92c4a0e2358f0fe
aliseylaneh <aliseylaneh@gmail.com>
2024-05-16 12:13:23 +0330

    GET store api added exception

--- src/presentation/apis.py
+++ src/presentation/apis.py
@@ -1,11 +1,13 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from domain.models import Store
 from services.store import GetStoreQueryService
 
 router = APIRouter()
 
 
-@router.get("/store/{id}", response_model=Store)
+@router.get("/store/{pk}", response_model=Store, )
 async def get_store(pk: int, service: GetStoreQueryService = Depends(GetStoreQueryService)):
     query = await service.handle(pk=pk)
+    if not query:
+        raise HTTPException(status_code=400, detail="Given id store not found!!")
     return query
