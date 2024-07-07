421138ce8e9a04e799e47ffa17ec42a7a2a748a3
aliseylaneh <aliseylaneh@gmail.com>
2024-07-07 11:41:50 +0330

    chnage implementation of inventory repository

--- internal/modules/invenotry/dependencies/inventory.py
+++ internal/modules/invenotry/dependencies/inventory.py
@@ -1,9 +1,8 @@
 from fastapi import Depends
 
+from internal.domain.interfaces.repositories.iinventory import IInventoryRepository
 from internal.modules.invenotry.aggregates.inventory import InventoryAggregate
-from internal.modules.invenotry.repository.elasticsearch_inventory import InventoryElasticSearchCommandRepository
 
 
-def inventory_aggregate(event_repo=Depends(InventoryElasticSearchCommandRepository),
-                        write_repo=Depends(InventoryElasticSearchCommandRepository)) -> InventoryAggregate:
-    return InventoryAggregate(write_repository=write_repo, event_repository=event_repo)
+def inventory_aggregate(repository=Depends(IInventoryRepository)) -> InventoryAggregate:
+    return InventoryAggregate(repository=repository)
--- internal/modules/invenotry/repository/elasticsearch_inventory.py
+++ internal/modules/invenotry/repository/elasticsearch_inventory.py
@@ -1,25 +1,20 @@
 import uuid
 
 from internal.domain.entities.inventory import Inventory
-from internal.domain.interfaces.repositories.iinventory import IInventoryRepository, IInventoryQueryRepository
+from internal.domain.interfaces.repositories.iinventory import IInventoryRepository
 
 
 class InventoryElasticSearchCommandRepository(IInventoryRepository):
 
-    def reserve(self, inventory: Inventory) -> Inventory:
-        pass
-
     def create(self, inventory: Inventory) -> Inventory:
         pass
 
-    def increase_soh(self, inventory: Inventory) -> Inventory:
+    def update_soh(self, inventory: Inventory) -> Inventory:
         pass
 
-    def decrease_soh(self, inventory: Inventory) -> Inventory:
+    def reserve(self, inventory: Inventory) -> Inventory:
         pass
 
-
-class InventoryElasticSearchQueryRepository(IInventoryQueryRepository):
     def find_by_id(self, pk: uuid) -> Inventory | None:
         pass
 
